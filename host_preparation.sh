ad18cceff39c166aff53f3c2c3d7b6d057870ba1
mkobayas <mkobayas@redhat.com>
2018-06-23 14:16:35 -0400

    add

---
+++ host_preparation.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+if [ $# -ne 3 ]; then
+  echo "host_preparation.sh <username> <password> <pool>"
+  exit 1
+fi
+
+base_dir=$(cd $(dirname $0); pwd)
+mkdir -p $base_dir/log
+
+########################################
+# host list
+########################################
+
+bastion=oselab.example.com
+
+nodes=(loadbalancer1.example.com \
+       master1.example.com \
+       master2.example.com \
+       master3.example.com \
+       infranode1.example.com \
+       infranode2.example.com \
+       infranode3.example.com \
+       node1.example.com \
+       node2.example.com \
+       node3.example.com \
+       node4.example.com \
+       node5.example.com \
+       node6.example.com)
+
+echo "Host Preparation ${nodes[@]}"
+
+
+#######################################################
+# Configure and install package for OCP cluster hosts
+#######################################################
+
+echo "Host Preparation ${nodes[@]}"
+pids=()
+for node in ${nodes[@]} 
+do
+   echo "===== config $node ===="
+   scp $base_dir/config_rhsm_work.sh $node:/root/host_preparation_work.sh
+   ssh -o StrictHostKeyChecking=no $node chmod +x /root/host_preparation_work.sh
+   ssh -o StrictHostKeyChecking=no $node "/root/host_preparation_work.sh $1 $2 $3" &> $base_dir/log/$node.log &
+   pids+=($!)
+done
+
+
+######################################################
+# Configure bastion
+######################################################
+
+$base_dir/bastion_preparation_.sh $1 $2 $3
+
+
+######################################################
+# wait host_preparation_work
+######################################################
+echo "wait host_preparation_work"
+for pid in ${pids[@]}; do
+  wait $pid
+done
+
+tail -n 2  $base_dir/log/*
---
+++ host_preparation_work.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+if [ -f /etc/rhsm/rhsm.conf.rpmnew ]; then
+  cat /etc/rhsm/rhsm.conf.rpmnew > /etc/rhsm/rhsm.conf
+fi
+
+if [ -f /etc/yum.repos.d/open.repo ]; then
+  rm -f /etc/yum.repos.d/open.repo
+fi
+
+subscription-manager register --username=$1 --password=$2
+
+subscription-manager refresh
+
+subscription-manager attach --pool=$3
+
+subscription-manager repos --disable="*"
+
+subscription-manager repos \
+    --enable="rhel-7-server-rpms" \
+    --enable="rhel-7-server-extras-rpms" \
+    --enable="rhel-7-server-ose-3.9-rpms" \
+    --enable="rhel-7-fast-datapath-rpms" \
+    --enable="rhel-7-server-ansible-2.4-rpms"
+
+yum clean all
+yum -y update
+
+yum install -y wget git net-tools bind-utils iptables-services bridge-utils bash-completion kexec-tools sos psacct vim NetworkManager docker-1.13.1
+systemctl enable docker
+yum -y update
+
+echo "====== FINISH $HOSTNAME ====="
+
+systemctl reboot
+
